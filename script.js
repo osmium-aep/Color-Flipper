492a14da516ad04b6b75aa4e6bd316a14be1208e
osmium-aep <osmium.aep@gmail.com>
2023-12-03 23:18:45 +0530

    Only Logic is Done

---
+++ script.js
@@ -0,0 +1,48 @@
+const flipperBtn = document.getElementsByTagName('button');
+let hex = "";
+let hexLetters = [
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    "A",
+    "B",
+    "C",
+    "E",
+    "F"
+]
+
+let randomNum = (arrLength) => {
+    arrLength = parseInt(arrLength)
+    return Math.floor(Math.random() * arrLength)
+}
+
+let randomHexGenerator = () => {
+    let hex = `${hexLetters[randomNum(hexLetters.length)]}${hexLetters[randomNum(hexLetters.length)]}${hexLetters[randomNum(hexLetters.length)]}${hexLetters[randomNum(hexLetters.length)]}${hexLetters[randomNum(hexLetters.length)]}${hexLetters[randomNum(hexLetters.length)]}`;
+    console.log(hex)
+    return "#" + hex
+}
+
+let colorFlip = ()=>{
+    hex = randomHexGenerator();
+    document.body.style.backgroundColor = hex;
+    document.querySelector('.colorText').innerHTML = hex;
+}
+
+// setInterval(()=>{
+//     colorFlip()
+// },1000)
+
+addEventListener('click', (elem)=>{
+    if(elem.target.innerHTML === "Flip"){
+       colorFlip() 
+    }
+})
+
+       
